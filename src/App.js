bac9d40dfcd6af892e1486c4feee1b01005ae23d
Jan Erik Askjellrud <jan.erik.askjellrud@catenda.no>
2025-08-12 13:47:46 +0200

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/hello?age=44');
+        setData(response.data);
+      } catch (error) {
+        console.error('API error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Result: {data}
         </a>
       </header>
     </div>
